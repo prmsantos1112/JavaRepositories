35b3271b0fda62908f61efb9d0b65e5fa0e8c28a
prmsantos1112 <prmsantosster@gmail.com>
2019-01-06 14:17:16 -0200

    Polimorfismo - Java Repositories - Faculdade

---
+++ src/Aplication/ProgramPolimorfismo.java
@@ -0,0 +1,30 @@
+package Aplication;
+
+import entities.Account;
+import entities.SavingsAccountSobrePosicao;
+
+public class ProgramPolimorfismo {
+
+	public static void main(String[] args) {
+		
+		/*               P O L I M O R F I S M O
+		 * Variáveis do mesmo tipo apontam para Objetos do tipo
+		 * específicos diferentes tendo comportamentos diferentes
+		 * conforme cada tipo específico; 
+		 */
+		
+		     // Conta Comum e ContaPoupança - Conta Poupança não desconta taxa de saque;
+		
+		Account polimorf_1 = new Account(1020, "Paulo Ricardo", 1000.0);
+		Account polimorf_2 = new SavingsAccountSobrePosicao(1023, "Maria", 1000.0, 0.01);
+		
+		polimorf_1.withdraw(50.0); 
+		polimorf_2.withdraw(50.0);
+		
+		
+		System.out.println(polimorf_1.getBalance());
+		System.out.println(polimorf_2.getBalance());
+
+	}
+
+}
